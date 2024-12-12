cf8ca88912f1a04dca411bae4509810bb946c9c4
Duy Van <duy5a247@gmail.com>
2024-12-12 14:13:22 +0700

    remove empty line of locators

--- app/Message/locator/main/main_message_loc.py
+++ app/Message/locator/main/main_message_loc.py
@@ -1,6 +1,5 @@
 message_app_package = "com.samsung.android.messaging"
 message_app_activity = "com.android.mms.ui.ConversationComposer"
-
 create_new_message = "//*[@content-desc='Compose new message']"
 
 
--- app/Message/locator/message/message_loc.py
+++ app/Message/locator/message/message_loc.py
@@ -1,9 +1,7 @@
 recipient_field= "//*[contains(@resource-id,'recipients_editor_to')]"
 contact_search_result = "//*[contains(@resource-id,'bubble_list_view')]/android.widget.LinearLayout"
-
 message_field= "//*[@resource-id='com.samsung.android.messaging:id/message_edit_text']"
 send_button = "//*[@content-desc='Send']"
-
 conversation_setting = "//*[@content-desc='Conversation settings']"
 delete_message = "//*[@text='Delete message']"
 delete_button = "//*[@content-desc='Delete, Button']"
